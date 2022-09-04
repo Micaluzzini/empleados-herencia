e7b0a0b94964fce2848fa1ed22c635c992aef957
Micaluzzini <micaweilenmann@gmail.com>
2022-09-04 10:26:52 -0300

    Update empleadoEventual.py
    se eliminaron métodos iguales entre clases hijas

--- empleadoEventual.py
+++ empleadoEventual.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from empleado import Empleado
+
 class EmpleadoEventual:
     def __init__(self, nombre, apellido, dni, salario, ventas):
         self.nombre = nombre
@@ -20,18 +22,4 @@ class EmpleadoEventual:
         comision = total_ventas * porcentaje_comision
         return comision
 
-    def calcular_ingreso_total(self):
-        ingreso_total = self.salario + self.calcular_comision()
-        return ingreso_total
-
-    def coincide(self, texto_a_buscar):
-        if texto_a_buscar in self.nombre or texto_a_buscar in self.apellido:
-            return True
-        else:
-            return False
-
-    def mostrar_datos(self):
-        texto = f"Nombre y apellido: {self.nombre} {self.apellido}\n"
-        texto += f"DNI: {self.dni} - Salario: {self.salario}\n"
-        texto += f"Ventas: {self.ventas}\n"
-        return texto
+    
